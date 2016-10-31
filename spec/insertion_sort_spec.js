0c57a718073a023aae1855dc75e9e6611d238132
Congwen Ma <Congwen.Ma@centro.net>
2016-10-31 11:32:58 -0500

    insertion sort, datastructure in javascript

---
+++ spec/insertion_sort_spec.js
@@ -0,0 +1,22 @@
+import surgeonkit from 'surgeonkit';
+import insertionSort from '../algorithms/insertion_sort';
+import { ascendingVerifier } from './order_verifier';
+
+var scrambledNumbers = [4,1,2,3,5];
+describe('#insertionSort', () => {
+  it ('should sort numbers', () => {
+    expect(insertionSort(scrambledNumbers)).toEqual([1,2,3,4,5])
+  })
+
+  it('should sort 100 numbers', () => {
+    expect(ascendingVerifier(
+      insertionSort(
+        surgeonkit.expand(20).map(n => (Math.random() * 100) | 0)
+      )
+    )).toBe(true)
+  })
+
+  it('should sort objects based on key', () => {
+    // copy out of the other one
+  })
+})
