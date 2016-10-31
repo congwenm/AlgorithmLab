ad0ac2e24d9f71c26bd90273da82f450596abf52
Fei <lostaznfound@gmail.com>
2016-10-31 18:19:29 -0500

    amortize wip

--- amortization/amortize.js
+++ amortization/amortize.js
@@ -4,13 +4,16 @@ import surgeonkit from 'surgeonkit';
 var Benchmark = require('benchmark')
 var filename = process.argv.slice(2)[0];
 // import fn from `./algorithms/${filename}`;
-var fn = require(`../algorithms/${filename}.js`).default;
+var method = require(`../algorithms/${filename}.js`).default;
 
 
 function run() {
-  console.log(fn(
+  var result = method(
     surgeonkit.expand(20).map(n => Math.ceil(Math.random() * 100))
-  ))
+  )
+
+  console.log(result);
+  
 }
 
 run();
