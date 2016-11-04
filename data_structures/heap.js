fbfb64902b578b6e371a74e39e148bd30ac9b242
Fei <lostaznfound@gmail.com>
2016-11-04 01:08:13 -0500

    wip heap

--- data_structures/heap.js
+++ data_structures/heap.js
@@ -1,3 +1,29 @@
-export default function Heap() {
+const { floor, log, pow } = Math
 
+export default function Heap() {
+  // we assume that array starts with index 1, 
+  // so `left_child` is `2k`, and `right_child` is `2k + 1`,  parent at `k/2`
+  this.q = [null]; 
+}
+
+Heap.prototype.insert = function(new_value) {
+  this.q[this.q.length]
+  this.bubble_up()
+}
+
+Heap.prototype.bubble_up = function(p) {
+  if (this.p) === -1) return; // at root of heap, no parent
+  if(this.getParent(p))
+}
+
+Heap.prototype.getParent = function(n) {
+  if (n === 1) {
+    return -1
+  } else {
+    return floor(n / 2);
+  }
+}
+
+Heap.prototype.getChildren() = function(n) {
+  return 2 * n;
 }
--- spec/data_structures/heap_spec.js
+++ spec/data_structures/heap_spec.js
@@ -1,4 +1,16 @@
-import heap from '../../data_structures/heap.js'
-describe('#heap', () => {
-  
+import Heap from '../../data_structures/heap.js'
+fdescribe('#heap', () => {
+  var heap;
+  beforeEach(() => {
+    heap = new Heap()
+    heap.insert(1)
+    heap.insert(2)
+    heap.insert(3)
+  })    
+
+  it('expect heap to be 3, 2, 1', function() {
+    expect(heap.q[0].value).toBe(3)
+    expect(heap.q[1].value).toBe(2)
+    expect(heap.q[2].value).toBe(1)
+  })
 })
