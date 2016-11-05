6fb2a1f27e7034e270212dec574370d01049be21
Fei <lostaznfound@gmail.com>
2016-11-05 12:02:26 -0500

    fixup! wip heap

--- data_structures/heap.js
+++ data_structures/heap.js
@@ -1,29 +1,66 @@
 const { floor, log, pow } = Math
+import util from '../util'
 
-export default function Heap() {
+// by default implemented a Max-Heap
+// can subclass ES6 Array? perhaps?
+export default class Heap {
   // we assume that array starts with index 1, 
   // so `left_child` is `2k`, and `right_child` is `2k + 1`,  parent at `k/2`
-  this.q = [null]; 
-}
-
-Heap.prototype.insert = function(new_value) {
-  this.q[this.q.length]
-  this.bubble_up()
-}
-
-Heap.prototype.bubble_up = function(p) {
-  if (this.p) === -1) return; // at root of heap, no parent
-  if(this.getParent(p))
-}
-
-Heap.prototype.getParent = function(n) {
-  if (n === 1) {
-    return -1
-  } else {
-    return floor(n / 2);
+  constructor () {
+    this.queue = [null]; 
   }
-}
 
-Heap.prototype.getChildren() = function(n) {
-  return 2 * n;
+  // get position of the parent
+  parent_indice (n) {
+    if (n === 1) {return -1 } 
+    else { return floor(n / 2) }
+  }
+  
+  // get children
+  left_child (n) {return 2 * n; }
+  right_child (n) {return 2 * n + 1; }
+
+  insert (new_value) {
+    // console.log('-----Insertion Start------')
+    this.queue.push(new_value);
+    // console.log('inserting', new_value, this.queue)
+    this.bubble_up(this.queue.length - 1)
+    // console.log('-----Insertion Complete------')
+  }
+
+  // main controller of orders
+  // {p} = index of current elem
+  bubble_up (p) {
+    // console.log('bubble_up is called with child index', p)
+    // console.log(`parent_index: ${this.parent_indice(p)}, child_indx: ${p}`)
+    
+    var parentIndex = this.parent_indice(p)
+    // at root of heap, no need to bubble
+    if (parentIndex === -1) { return null; }   
+
+    // < is max_heap, > is min_heap
+    // console.log(`compare values, ${this.queue[parentIndex]} vs. ${this.queue[p]}`)
+    if (this.queue[parentIndex] < this.queue[p]) {
+      this.swap(p, parentIndex)
+      this.bubble_up(this.parent_indice(p))
+    }
+  }
+
+  swap(i1, i2) {
+    // console.log(`swapping ${i1} with ${i2}`)
+    var tmp = this.queue[i1]
+    this.queue[i1] = this.queue[i2]
+    this.queue[i2] = tmp
+  }
+
+  print() {
+    var arr = this.queue.slice();
+    arr.splice(0, 1) // get rid of first padding elem (null)
+
+    // j will be length of second to bottom level when loop terminates
+    for (let i = 0, j = 1; arr.length; j = Math.pow(2, ++i))  {
+      var splicedPiece = arr.splice(0, j)
+      console.log(`level ${util.padding(i+1, 3)}  ->  ${splicedPiece}`)
+    }
+  }
 }
--- spec/data_structures/heap_spec.js
+++ spec/data_structures/heap_spec.js
@@ -1,16 +1,37 @@
-import Heap from '../../data_structures/heap.js'
+import Heap from '../../data_structures/heap'
+import surgeonkit from 'surgeonkit'
+import util from '../../util'
+const { reverse_order, ordered } = util
+const { expand } = surgeonkit
+
 fdescribe('#heap', () => {
   var heap;
   beforeEach(() => {
     heap = new Heap()
-    heap.insert(1)
-    heap.insert(2)
-    heap.insert(3)
+    // heap.print.call({ queue: expand(17) })
   })    
 
-  it('expect heap to be 3, 2, 1', function() {
-    expect(heap.q[0].value).toBe(3)
-    expect(heap.q[1].value).toBe(2)
-    expect(heap.q[2].value).toBe(1)
+  it('expect heap to be 4, 3, 2, 1', function() {
+    heap.insert(1)
+    heap.insert(2) // swap 2 with 1
+    heap.insert(3) // swap 3 with 2
+    heap.insert(4) // swap 4 with 1, 4 with 3
+    // heap.print()
+    expect(heap.queue[1]).toBe(4) 
+    expect(heap.queue[2]).toBe(3)
+    expect(heap.queue[3]).toBe(2)
+    expect(heap.queue[4]).toBe(1)
   })
+
+  it('expect heap to not swap at all if inserted at reverse_order', function() {
+    var rev_arr = reverse_order(10)
+    rev_arr.forEach(n => heap.insert(n))
+    expect(heap.queue).toEqual([null, ...rev_arr])
+  })
+
+  it('expect heap largest to be in index 1', function() {
+    var orded_arr = ordered(10)
+    orded_arr.forEach(n => heap.insert(n))
+    expect(Math.max(...heap.queue)).toEqual(heap.queue[1])
+  })  
 })
