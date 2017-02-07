438184977c5dbdd0de8446d23f66086f68ffa3c0
Congwen Ma <Congwen.Ma@centro.net>
2017-02-07 13:00:48 -0600

    --amend

--- ruby/select_vs_amp.rb
+++ ruby/select_vs_amp.rb
@@ -2,8 +2,8 @@ require 'benchmark'
 
 n = 50_000
 
-arr = %w(hello world all of you)
-match = %w(hello)
+arr = %w(hello world all of you kajdf adfs asd fa sdf sadf sad fasd fasdf adsf)
+match = %w(hello world)
 
 Benchmark.bm(1) do |x|
   x.report do
@@ -15,6 +15,12 @@ Benchmark.bm(1) do |x|
   x.report do
     for i in 1..n
       arr & match
+    end
+  end
+
+  x.report do
+    for i in 1..n
+      arr.select{|s| %w(hello).include?(s)}
     end
   end
 end
