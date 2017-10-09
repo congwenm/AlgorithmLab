7cbf5ffca50e4999b45f774b7db1a224d360d3c5
Congwen Ma <lostaznfound@gmail.com>
2017-10-09 16:29:37 -0500

    restructure

--- ruby/try_vs_respond_to.rb
+++ ruby/try_vs_respond_to.rb
@@ -1,21 +1,23 @@
-require 'benchmark'
+require 'benchmark/ips'
 require 'active_support/all'
 
 class MyClass; include ActiveSupport; end
 obj = MyClass.new
 
-n = 50_000
 
-Benchmark.bm(7) do |x|
-  x.report do
-    for i in 1..n
-      obj.try(:name)
-    end
+Benchmark.ips do |x|
+  x.report 'try' do
+    obj.try(:name)
   end
 
-  x.report do
-    for i in 1..n
-      obj.name if obj.respond_to?(:name)
-    end
+  x.report 'respond_to' do
+    obj.name if obj.respond_to?(:name)
   end
 end
+
+# Warming up --------------------------------------
+#                   try             188.746k i/100ms
+#                   respond_to      241.457k i/100ms
+# Calculating -------------------------------------
+#                   try             2.949M (±12.5%) i/s -     14.533M in   5.014803s
+#                   respond_to      6.508M (±16.1%) i/s -     31.389M in   5.017087s
